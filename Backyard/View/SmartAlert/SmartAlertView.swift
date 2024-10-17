5bbaf0fd9812e0ed71dd280eb0b945a9149e09a2
Sxy623 <shenxinyi@zju.edu.cn>
2024-10-17 23:27:37 +0800

    Merge Fix

--- Backyard/View/SmartAlert/SmartAlertView.swift
+++ Backyard/View/SmartAlert/SmartAlertView.swift
@@ -113,16 +113,6 @@ struct CameraButton: View {
     }
 }
 
-extension Color {
-    init(hex: UInt) {
-        self.init(
-            red: Double((hex >> 16) & 0xFF) / 255.0,
-            green: Double((hex >> 8) & 0xFF) / 255.0,
-            blue: Double(hex & 0xFF) / 255.0
-        )
-    }
-}
-
 #Preview {
     SmartAlertView()
 }
